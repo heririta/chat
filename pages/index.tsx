77944ec2fead4aa608ff192133280fe09724a481
herisan <68097716+heririta@users.noreply.github.com>
2024-01-28 18:12:38 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,7 +58,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header />
+      
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
         <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
           Ask anything.
@@ -116,7 +116,7 @@ const Home: NextPage = () => {
           )}
         </div>
       </main>
-      <Footer />
+      
     </div>
   );
 };
